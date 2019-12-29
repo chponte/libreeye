cdaa5cac8cc81c06c1db3a7d529983fd354ccbcd
Christian Ponte <christian.ponte@udc.es>
2019-12-29 14:57:45 +0100

    Apply proper licensing to project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Empty readme
+# Libreeye
+
+WIP
--- src/libreeye/fs/local.py
+++ src/libreeye/fs/local.py
@@ -1,3 +1,19 @@
+# This file is part of Libreeye.
+# Copyright (C) 2019 by Christian Ponte
+#
+# Libreeye is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Libreeye is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Libreeye. If not, see <http://www.gnu.org/licenses/>.
+
 from libreeye.fs.base import ItemStorage, Item
 import logging
 import os
